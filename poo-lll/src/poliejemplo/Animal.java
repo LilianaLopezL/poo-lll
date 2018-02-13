01184e5edaaac5d680c112120a4470f58666410c
T-102 <T-102@PC251>
2018-02-12 19:46:54 -0600

    POLIMORFISMO

---
+++ poo-lll/src/poliejemplo/Animal.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poliejemplo;
+
+/**
+ *
+ * @author T-102
+ */
+public class Animal {
+    private String color;
+    private int Edad;
+
+    /**
+     * @return the color
+     */
+    public String getColor() {
+        return color;
+    }
+
+    /**
+     * @param color the color to set
+     */
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    /**
+     * @return the Edad
+     */
+    public int getEdad() {
+        return Edad;
+    }
+
+    /**
+     * @param Edad the Edad to set
+     */
+    public void setEdad(int Edad) {
+        this.Edad = Edad;
+    }
+    
+    
+    
+    
+}
